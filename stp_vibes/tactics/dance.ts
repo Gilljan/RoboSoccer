63459faf59d95242bcfcb52b63867cea5ee3db0c
linas <li.meinecke@ostfalia.de>
2023-12-28 17:47:29 +0100

    Count test

--- stp_vibes/tactics/dance.ts
+++ stp_vibes/tactics/dance.ts
@@ -1,6 +1,8 @@
 import {FriendlyRobot} from "../../base/robot";
-import {MoveTo} from "../skills/moveto";
+import {MoveTo} from "stp_vibes/skills/moveto";
 import {Vector} from "../../base/vector";
+
+let count : number = 0;
 
 export class Dance {
     private robots : FriendlyRobot[];
@@ -13,10 +15,12 @@ export class Dance {
         this.robots.forEach((value, index, array) => {
             const skill = new MoveTo(value);
 
-            const y = Math.sin(index / this.robots.length * Math.PI * 2);
-            const x = Math.cos(index / this.robots.length * Math.PI * 2);
+            const y = Math.sin(index / this.robots.length * Math.PI * count);
+            const x = Math.cos(index / this.robots.length * Math.PI * count);
 
             skill.run(new Vector(x, y), 0);
         })
+
+        count++;
     }
 }
