714244c27a20c9d706061bac8ccb31e1d8cb8a1c
linas <li.meinecke@ostfalia.de>
2023-12-30 18:12:22 +0100

    move keeper towards ball

--- stp_vibes/plays/penaltydefense.ts
+++ stp_vibes/plays/penaltydefense.ts
@@ -15,16 +15,25 @@ export class PenaltyDefense {
         const robot = World.FriendlyRobotsById[0];
 
         const play = new MoveTo(robot);
-        
-        if (count % 200 == 0) {
-        
-        const min = 0;
-        const max = 17;
-        random = (Math.floor(Math.random() * (max - min + 1)) + min - 10) / 10;
 
+        if (count % 200 == 0) {
+
+            const min = 0;
+            const max = 17;
+
+            if (vectorDistance(robot.pos, World.Ball.pos) < 2.0) {
+                play.run(new Vector(World.Ball.pos.x, robot.pos.y), 0);
+
+            } else {
+                random = (Math.floor(Math.random() * (max - min + 1)) + min - 10) / 10;
+                play.run(new Vector(random, robot.pos.y), 0);
+            }
         }
-        play.run(new Vector(0.0 + random, robot.pos.y), 0);
-        
+
         count++;
     }
+}
+
+function vectorDistance(vec1: Vector, vec2: Vector): number {
+    return Math.abs(Math.pow(vec1.x - vec2.x, 2) + Math.pow(vec1.y - vec2.y, 2));
 }
