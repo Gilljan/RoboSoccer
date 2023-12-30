34e4a10552af507a994e86fad8e914c5c1775308
linas <li.meinecke@ostfalia.de>
2023-12-30 18:38:35 +0100

    move into end state

--- stp_vibes/plays/penaltydefense.ts
+++ stp_vibes/plays/penaltydefense.ts
@@ -1,6 +1,9 @@
 import * as World from "base/world";
 import {MoveTo} from "stp_vibes/skills/moveto";
 import {Vector} from "base/vector";
+import * as Game from "./game";
+
+let started: boolean = false;
 
 let count: number = 0;
 let targetX: number = 0;
@@ -16,12 +19,15 @@ export class PenaltyDefense {
 
         const play = new MoveTo(robot);
 
+        if (!started) {
+            started = World.Ball.speed.x != 0 || World.Ball.speed.y != 0;
+        }
+
         if (count % 50 == 0) {
 
             const min = 0;
             const max = 10;
 
-amun.log(vectorDistance(robot.pos, World.Ball.pos) < 2.0);
             if (vectorDistance(robot.pos, World.Ball.pos) < 3.0) {
                  targetX = World.Ball.pos.x
 
@@ -34,6 +40,14 @@ amun.log(vectorDistance(robot.pos, World.Ball.pos) < 2.0);
         play.run(new Vector(targetX, robot.pos.y), 0);
 
         count++;
+
+        if (started && World.Ball.pos.equals(new Vector(0, 0))) {
+            if (Game.currentGameState == Game.GameState.BShoot) {
+                (Game.currentGameState as any) = Game.GameState.BEnd;
+            } else {
+                (Game.currentGameState as any) = Game.GameState.YEnd;
+            }
+        }
     }
 }
 
