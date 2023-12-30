87f9a80c343a21f2f743c8830708c0a413ae7903
linas <li.meinecke@ostfalia.de>
2023-12-30 13:49:40 +0100

    reduce duplicated code

--- stp_vibes/tactics/dance.ts
+++ stp_vibes/tactics/dance.ts
@@ -2,6 +2,8 @@ import {FriendlyRobot} from "../../base/robot";
 import {MoveTo} from "stp_vibes/skills/moveto";
 import {Vector} from "base/vector";
 
+let centerX: number = 0;
+let centerY: number = 0;
 
 let count: number = 0;
 
@@ -20,70 +22,41 @@ export class Dance {
 
     constructor(robots: FriendlyRobot[]) {
         this.robots = robots;
-        //this.robots.shift();
-
     }
 
     public run() {
-
         switch (currentPhase) {
             case DancePhase.Circle: {
-                const centerX = 0; // Set the X-coordinate of the center
-                const centerY = 0; // Set the Y-coordinate of the center
-
-                //amun.log(this.robots);
-
                 this.robots.forEach((value, index, array) => {
                     const skill = new MoveTo(value);
 
-                    if (index == 0) {
-                        skill.run(new Vector(0.0, 4.0), 0);
-
+                    if (this.moveKeeper(value, index)) {
                         return;
                     }
 
-
-                    // Calculate the position in a circle around the center
-                    const angle = ((index + count / 500) / this.robots.length) * 2 * Math.PI;
-                    const radius = 2 + Math.cos(count / 500); // You can adjust the radius as needed
-
-                    const x = centerX + radius * Math.cos(angle);
-                    const y = centerY + radius * Math.sin(angle);
+                    const vec = this.calcRotatedPos(index, false);
 
                     // Use the count variable to control the rotation
                     const orientation = count * (index / this.robots.length) * 360;
 
-                    skill.run(new Vector(x, y), 0);
+                    skill.run(vec, 0);
                 });
                 break;
             }
             case DancePhase.ReverseCircle: {
-                const centerX = 0; // Set the X-coordinate of the center
-                const centerY = 0; // Set the Y-coordinate of the center
-
-                //amun.log(this.robots);
-
                 this.robots.forEach((value, index, array) => {
                     const skill = new MoveTo(value);
 
-                    if (index == 0) {
-                        skill.run(new Vector(0.0, 4.0), 0);
-
+                    if (this.moveKeeper(value, index)) {
                         return;
                     }
 
-
-                    // Calculate the position in a circle around the center
-                    const angle = ((index + count / 500) / this.robots.length) * 2 * Math.PI;
-                    const radius = 2 + Math.cos(count / 500); // You can adjust the radius as needed
-
-                    const x = centerX + radius * Math.cos(angle);
-                    const y = centerY + radius * Math.sin(angle);
+                    const vec = this.calcRotatedPos(index, true);
 
                     // Use the count variable to control the rotation
                     const orientation = count * (index / this.robots.length) * 360;
 
-                    skill.run(new Vector(x, y), 0);
+                    skill.run(vec, 0);
                 });
             }
         }
@@ -93,11 +66,32 @@ export class Dance {
         if (count >= currentPhase) {
             amun.log(Object.values(DancePhase));
             amun.log(Object.values(DancePhase).indexOf(currentPhase));
-            currentPhase = Object.values(DancePhase)[Object.values(DancePhase).indexOf((currentPhase) + 1) % 3];
+            currentPhase = Object.values(DancePhase)[(currentPhase + 1) % 3];
 
             amun.log(currentPhase);
 
             count = 0;
         }
+    }
+
+    moveKeeper(robot: FriendlyRobot, index: number): boolean {
+        if (index == 0) {
+            new MoveTo(robot).run(new Vector(0.0, 4.0), 0);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    calcRotatedPos(index: number, reverse: boolean): Vector {
+        // Calculate the position in a circle around the center
+        const angle = ((index + (reverse ? -count : count) / 500) / this.robots.length) * 2 * Math.PI;
+        const radius = 2 + Math.cos(count / 500);
+
+        const x = centerX + radius * Math.cos(angle);
+        const y = centerY + radius * Math.sin(angle);
+
+        return new Vector(x, y);
     }
 }
